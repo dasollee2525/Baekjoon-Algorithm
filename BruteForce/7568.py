e7d1f9f7f663063d373f02e50b21005eec2007ec
dasollee2525 <90080808+dasollee2525@users.noreply.github.com>
2022-07-13 00:18:42 +0900

    Update 7568.py

--- BruteForce/7568.py
+++ BruteForce/7568.py
@@ -5,16 +5,19 @@ weight = []
 height = []
 order = []
 for i in range(num):
-  list.extend(map(int, input().split()))
+  list.extend(map(int, input().split())) //transform string input into int
 
 for j in range(num):
   weight.append(list[j*2])
   height.append(list[j*2+1])
 
-while(weight):
-  max_weight = max(weight)
-  max_height = max(height)
-  if weight.index(max_weight) == height.index(max_height):
-    
-    if(weight.count(max(weight)) > 1 and height.count(max(height)) > 1):
+for m in range(num): 
+  count = 1
+  for n in range(num):
+    if(weight[n] > weight[m] and height[n] > height[m]):
+      count = count + 1
+  order.append(count)
+  print(count, end=" ")
   
+ 
+//When the list is empty, it is impossible to insert the element with index 2 or others
